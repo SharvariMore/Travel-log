8beac696cb847b94efd0134fb320030011f24136
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2022-05-07 23:21:56 +0530

    Update AddForm.jsx

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -16,7 +16,7 @@ export default function AddForm({openPopup, setOpenPopup}) {
         <Dialog open={openPopup} className="popup">
             <DialogTitle>
                 <h1 className="title">Submit Data for Enquiry</h1>
-                <i><span style={{color: 'red'}}>Note</span>: We will get back to you thr phone & email.</i>
+                <i><span style={{color: 'red'}}>Note</span>: We will get back to you through phone & email.</i>
             </DialogTitle>
             <DialogContent className="content">
                 <form onSubmit={handleSubmit}>
@@ -38,6 +38,10 @@ export default function AddForm({openPopup, setOpenPopup}) {
                                 <label>Email-id: </label>
                                 <input type="email" placeholder="Enter email id" required></input>
                             </div>
+                            <div>
+                                <label>Enter No. of Persons: </label>
+                                <input type="number" placeholder="Enter no. of persons" required></input>
+                            </div>
                         </div>
                         <div className="right">
                             <div>
@@ -49,16 +53,25 @@ export default function AddForm({openPopup, setOpenPopup}) {
                                 <input type="text" placeholder="Enter your city" required></input>
                             </div>
                             <div>
+                                <label>Enter No. of Days: </label>
+                                <input type="number" placeholder="Enter no. of days" required></input>
+                            </div>
+                            <div>
                                 <label>Select Cars: </label>
                                 <select>
+                                    <option>Select Car </option>
                                     <option>Swift Dzire</option>
                                     <option>Ertiga</option>
                                     <option>Innova</option>
+                                    <option>Tavera</option>
+                                    <option>Xcent</option>
+                                    <option>Etios</option>
                                 </select>
                             </div>
                             <div>
                                 <label>Select Car Type: </label>
                                 <select>
+                                    <option>Select Type </option>
                                     <option>AC</option>
                                     <option>NON-AC</option>
                                 </select>
@@ -66,31 +79,11 @@ export default function AddForm({openPopup, setOpenPopup}) {
                         </div>
                     </div>
                     <button type="submit">Submit</button>
-                    <button type="cancel"
-                      onClick={() => setOpenPopup(false)}
-                    >Cancel</button>
+                    <button type="cancel" onClick={() => setOpenPopup(false)}>Cancel</button>
                 </form>
             </DialogContent>
         </Dialog>
     );
 }
 
-const Section = styled.section`
-button {
-    padding: 0.5rem 1rem;
-    cursor: pointer;
-    border-radius: 1rem;
-    border: none;
-    color: white;
-    background-color: #48cae4;
-    font-size: 1.1rem;
-    letter-spacing: 0.1rem;
-    text-transform: uppercase;
-    transition: 0.3s ease-in-out;
-    &:hover {
-      background-color: #023e8a;
-    }
-  }
-`;
 
-{/*<Link to="www.google.com" target="_blank"></Link>*/}
