c84529f23ea05f791b146d99ae8687abffd07484
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2022-05-10 17:21:55 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,200 +1,214 @@
-import React, {useState, useRef} from 'react';
-import styled from 'styled-components';
-import homeImage from '../assets/hero.png';
-import emailjs from 'emailjs-com';
+import React, {useState} from 'react';
+import styled from "styled-components";
+import logo from "../assets/logo.png";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { VscChromeClose } from "react-icons/vsc";
 import AddForm from './AddForm';
+import Connect from './Connect';
 import { Tooltip } from '@material-ui/core';
 
-export default function Hero() {
-
-  function getValue() {
-    var retVal = prompt("Enter your name : ", "your name here");
-    document.write("You have entered : " + retVal);
- }
-
-  const form = useRef();
-
-  const exploreDest = (e) => {
-    e.preventDefault();
-    emailjs.sendForm('service_rn86iqs', 'template_dwo37rs', form.current, 'BSXJF-kNz4yuSrMGQ')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-    window.alert("Dates Submitted Successfully!");
-    e.target.reset();
-  }
-  
+export default function Navbar () {
+  const [navbarState, setNavbarState] = useState(false);
+  const [openPopup, setOpenPopup] = useState(false);
+  const [popUp, setPopUp] = useState(false);
     return (
-    <>
-        <Section id="hero">
-          <div className="background">
-           <img src={homeImage} alt="" />   
-          </div>  
-          <div className="content">
-              <div className="title">
-                <h1>TRAVEL TO EXPLORE</h1>
-                <p>
-                  Let's Embark a New Journey to Start Upon! 
-                </p>  
-              </div>
-              <form ref={form} onSubmit={exploreDest}>
-              <div className="search">
-                  <div className="container">
-                      <label htmlFor="">Where You Want To Go?</label>
-                      <input type="text" placeholder="Search Your Location" name="place" required/>
-                  </div>
-                  <div className="container">
-                      <label htmlFor="">Check-In</label>
-                      <input type="date" name="from date" required/>
-                  </div>
-                  <div className="container">
-                      <label htmlFor="">Check-Out</label>
-                      <input type="date" name="to date" required/>
-                  </div>
-                  <div className="container">
-                      <input type="email" placeholder="Enter your email id here " name="email-id" onclick={getValue} required/>
-                  </div>
-                   
-                  <Tooltip title={<h4>Check Date Availability & then Register for Booking</h4>} arrow>
-                   <button type="submit">Explore Now!</button>
-                  </Tooltip>
-                    
+        <>
+            <Nav>
+             <div className="brand">
+                    <div className="container">
+                        <img src={logo} alt="" />
+                        <h1>Vikram Travels</h1>
+                    </div>
+                        <div className="toggle">
+                          {navbarState ? (
+                            <VscChromeClose onClick={() => setNavbarState(false)} />
+                          ): (
+                            <GiHamburgerMenu onClick={() => setNavbarState(true)} />
+                          )}
+                        </div>
+                </div>  
 
-              </div>
-              <AddForm />
-              </form>
-          </div>
-          
-        </Section>    
-    </>
+                <ul>
+                    <li>
+                        <a href="#home">HOME</a>
+                    </li>
+                    <li>
+                        <a href="#services">Services</a>
+                    </li>
+                    <li>
+                        <a href="#recommend">Places</a>
+                    </li>
+                    <li>
+                        <a href="#testimonials">Testimonials</a>
+                    </li>
+                    <li>
+                      <Tooltip title={<h4>Register after Date Confirmation</h4>} class="tooltip" arrow>
+                        <a onClick={()=>setOpenPopup(true)}><b>Register</b></a>
+                        </Tooltip>
+                    </li>
+                    <li>
+                        <a onClick={()=>setPopUp(true)}><b>Connect</b></a>
+                    </li>
+                </ul>
+
+                <AddForm
+                openPopup={openPopup}
+                setOpenPopup={setOpenPopup}
+                />
+
+                <Connect
+                popUp={popUp}
+                setPopUp={setPopUp}
+                />
+            </Nav>
+
+            <ResponsiveNav state={navbarState}>
+        <ul>
+          <li>
+            <a href="#home" onClick={() => setNavbarState(false)}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#services" onClick={() => setNavbarState(false)}>
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#recommend" onClick={() => setNavbarState(false)}>
+              Places
+            </a>
+          </li>
+          <li>
+            <a href="#testimonials" onClick={() => setNavbarState(false)}>
+              Testimonials
+            </a>
+          </li>
+          <li>
+            <a onClick={()=>setOpenPopup(true)}><b>Register</b></a>
+          </li>
+          <li>
+            <a onClick={()=>setPopUp(true)}><b>Connect</b></a>
+          </li>
+        </ul>
+      </ResponsiveNav>
+    
+        </>
     );
 }
 
-const Section = styled.section`
-  position: relative;
-  margin-top: 2rem;
-  width: 100%;
-  height: 100%;
-  .background {
-    height: 100%;
-    img {
-      width: 100%;
-      filter: brightness(60%);
-    }
-  }
-  
-  .content {
-    height: 100%;
-    width: 100%;
-    position: absolute;
-    top: 0;
-    z-index: 3;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 1rem;
-    .title {
-      color: white;
-      h1 {
-        font-size: 3rem;
-        letter-spacing: 0.2rem;
-      }
-      p {
-        text-align: center;
-        padding: 0 30vw;
-        margin-top: 0.5rem;
-        font-size: 1.2rem;
-      }
-    }
-    .search {
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .brand {
+    .container {
+      cursor: pointer;
       display: flex;
-      background-color: #ffffffce;
-      padding: 0.5rem;
-      border-radius: 0.5rem;
-      .container {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        padding: 0 1.5rem;
-        label {
-          font-size: 1.1rem;
-          color: #03045e;
+      justify-content: center;
+      align-items: center;
+      gap: 0.4rem;
+      font-size: 1.2rem;
+      font-weight: 900;
+      text-transform: uppercase;
+    }
+    .toggle {
+      display: none;
+    }
+  }
+  ul {
+    display: flex;
+    gap: 1rem;
+    list-style-type: none;
+    li {
+      a {
+        text-decoration: none;
+        cursor: pointer;
+        color: #0077b6;
+        font-size: 1.2rem;
+        transition: 0.1s ease-in-out;
+        &:hover {
+          color: #023e8a;
         }
-        input {
-          background-color: transparent;
-          border: none;
-          text-align: center;
-          color: black;
-          &[type="date"] {
-            padding-left: 0.3rem;
-          }
-          &::placeholder {
-            color: black;
-          }
-          &:focus {
-            outline: none;
-          }
+        #register {
+          border: bold;
         }
       }
-      button {
-        padding: 1rem;
-        cursor: pointer;
-        margin-left: 25px;
-        border-radius: 0.3rem;
-        border: none;
-        color: white;
-        background-color: #4361ee;
-        font-size: 1.1rem;
-        text-transform: uppercase;
-        transition: 0.3s ease-in-out;
-        &:hover {
-          background-color: #023e8a;
+      &:first-of-type {
+        a {
+          color: #023e8a;
+          font-weight: 900;
         }
       }
     }
   }
-  @media screen and (min-width: 280px) and (max-width: 980px) {
-    height: 25rem;
-    .background {
-      background-color: palegreen;
-      img {
-        height: 100%;
+  button {
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+    border-radius: 1rem;
+    border: none;
+    color: white;
+    background-color: #48cae4;
+    font-size: 1.1rem;
+    letter-spacing: 0.1rem;
+    text-transform: uppercase;
+    transition: 0.3s ease-in-out;
+    &:hover {
+      background-color: #023e8a;
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    .brand {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+      .toggle {
+        display: block;
       }
     }
-    .content {
-      .title {
-        h1 {
-          font-size: 1rem;
-        }
-        p {
-          font-size: 0.8rem;
-          padding: 1vw;
+    ul {
+      display: none;
+    }
+    button {
+      display: none;
+    }
+  }
+`;
+
+const ResponsiveNav = styled.div`
+  display: flex;
+  position: absolute;
+  z-index: 1;
+  top: ${({ state }) => (state ? "50px" : "-400px")};
+  background-color: white;
+  height: 30vh;
+  width: 100%;
+  align-items: center;
+  transition: 0.3s ease-in-out;
+  ul {
+    list-style-type: none;
+    width: 100%;
+    li {
+      width: 100%;
+      margin: 1rem 0;
+      margin-left: 2rem;
+      a {
+        text-decoration: none;
+        color: #0077b6;
+        font-size: 1.2rem;
+        transition: 0.1s ease-in-out;
+        &:hover {
+          color: #023e8a;
         }
       }
-      .search {
-        flex-direction: column;
-        padding: 0.8rem;
-        gap: 0.8rem; 
-        .container {
-          padding: 0 0.8rem;
-          input[type="date"] {
-            padding-left: 1rem;
-          }
-        }
-        button {
-          padding: 1rem;
-          font-size: 1rem;
+      &:first-of-type {
+        a {
+          color: #023e8a;
+          font-weight: 900;
         }
       }
     }
   }
 `;
-
-
-
 
